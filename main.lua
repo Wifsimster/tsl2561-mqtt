599e30a349424dc8654856249f62ace014dbaab6
bot <contact@arca-computing.fr>
2016-04-10 18:37:36 +0200

    Improve files

--- main.lua
+++ main.lua
@@ -21,13 +21,20 @@ TOPIC = "/sensors/bureau/tsl2561/data"
 -- Init client with keepalive timer 120sec
 m = mqtt.Client(CLIENT_ID, 120, "", "")
 
-tmr.alarm(0, 1000, 1, function()
+tmr.alarm(2, 1000, 1, function()
+    tmr.stop(2)
     print("Connecting to MQTT: "..BROKER_IP..":"..BROKER_PORT.."...")
-    tmr.stop(0)
     m:connect(BROKER_IP, BROKER_PORT, 0, function(conn)
         print("Connected to MQTT: "..BROKER_IP..":"..BROKER_PORT.." as "..CLIENT_ID)
         LUX_0 = tonumber(readLux()._0)
         LUX_1 = tonumber(readLux()._1)
+        
+        -- First send data
+        DATA = '{"lux_0":"'..LUX_0..'","lux_1":"'..LUX_1..'"}'
+        m:publish(TOPIC, DATA, 0, 0, function(conn)
+            print(CLIENT_ID.." sending data: "..DATA.." to "..TOPIC)
+        end)
+        
         -- Check every 5s for values change
         tmr.alarm(1, 5000, 1, function()
             TMP_LUX_0 = tonumber(readLux()._0)
