8bb67063600d85a335dd9df6f546b22ba629880e
bot <contact@arca-computing.fr>
2016-05-21 18:31:59 +0200

    Add ntp date time

--- main.lua
+++ main.lua
@@ -1,4 +1,7 @@
 require('config')
+local ntp = require('ntp')
+
+TOPIC = "/sensors/"..LOCATION.."/tsl2561/data"
 
 function readLux()
     tsl2561 = require("tsl2561")
@@ -12,43 +15,25 @@ function readLux()
     return rst
 end
 
--- MQTT client
-TOPIC = "/sensors/bureau/tsl2561/data"
-
 -- Init client with keepalive timer 120sec
 m = mqtt.Client(CLIENT_ID, 120, "", "")
 
-tmr.alarm(2, 1000, 1, function()
-    tmr.stop(2)
-    print("Connecting to MQTT: "..BROKER_IP..":"..BROKER_PORT.."...")
-    m:connect(BROKER_IP, BROKER_PORT, 0, function(conn)
-        print("Connected to MQTT: "..BROKER_IP..":"..BROKER_PORT.." as "..CLIENT_ID)
-        LUX_0 = tonumber(readLux()._0)
-        LUX_1 = tonumber(readLux()._1)
-        
-        -- First send data
-        DATA = '{"mac":"'..wifi.sta.getmac()..'", "ip":"'..wifi.sta.getip()..'",'
+-- Sync to NTP server
+ntp.sync()
+
+print("Connecting to MQTT: "..BROKER_IP..":"..BROKER_PORT.."...")
+m:connect(BROKER_IP, BROKER_PORT, 0, 1, function(conn)
+    print("Connected to MQTT: "..BROKER_IP..":"..BROKER_PORT.." as "..CLIENT_ID)
+    tmr.alarm(1, REFRESH_RATE, 1, function()
+        data = readLux()
+        LUX_0 = tonumber(data._0)
+        LUX_1 = tonumber(data._1)
+        DATA = '{"mac":"'..wifi.sta.getmac()..'","ip":"'..wifi.sta.getip()..'",'
+        DATA = DATA..'"date":"'..ntp.date()..'","time":"'..ntp.time()..'",'
         DATA = DATA..'"lux_0":"'..LUX_0..'","lux_1":"'..LUX_1..'"}'
+        -- Publish a message (QoS = 0, retain = 0)       
         m:publish(TOPIC, DATA, 0, 0, function(conn)
             print(CLIENT_ID.." sending data: "..DATA.." to "..TOPIC)
         end)
-        
-        -- Check every 5s for values change
-        tmr.alarm(1, REFRESH_RATE, 1, function()
-            TMP_LUX_0 = tonumber(readLux()._0)
-            TMP_LUX_1 = tonumber(readLux()._1)
-            if(LUX_0 ~= TMP_LUX_0 or LUX_1 ~= TMP_LUX_1) then
-                DATA = '{"mac":"'..wifi.sta.getmac()..'", "ip":"'..wifi.sta.getip()..'",'
-                DATA = DATA..'"lux_0":"'..TMP_LUX_0..'","lux_1":"'..TMP_LUX_1..'"}'
-                -- Publish a message (QoS = 0, retain = 0)       
-                m:publish(TOPIC, DATA, 0, 0, function(conn)
-                    print(CLIENT_ID.." sending data: "..DATA.." to "..TOPIC)
-                end)
-            else
-                print("No change in value, no data send to broker.")
-            end
-        end)
     end)
 end)
-
-m:close();
