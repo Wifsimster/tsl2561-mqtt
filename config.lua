5e3243de7997f894e6c0c9ab9164670739f6a47d
bot <contact@arca-computing.fr>
2016-05-22 19:40:59 +0200

    config change

--- config.lua
+++ config.lua
@@ -6,8 +6,11 @@ PWD = "192Wifsimster!!"
 BROKER_IP = "192.168.0.35"
 BROKER_PORT = 1883
 CLIENT_ID = "ESP8266-"..node.chipid()
-REFRESH_RATE = 30000
+REFRESH_RATE = 30000 -- ms
 
 -- GPIO
 SDA_PIN = 3 -- GPIO_0
 SCL_PIN = 4 -- GPIO_2
+
+-- LOCATION
+LOCATION = "livingroom"
