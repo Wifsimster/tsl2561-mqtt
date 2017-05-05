49c804cb11b2bcc76da4a4c92f3bf5727f7d7c07
Wifsimster <wifsimster@gmail.com>
2017-05-05 15:12:29 +0200

    Add online message & real time messaging

--- config.lua
+++ config.lua
@@ -6,7 +6,7 @@ PWD = "192Wifsimster!!"
 BROKER_IP = "192.168.0.35"
 BROKER_PORT = 1883
 CLIENT_ID = "ESP8266-"..node.chipid()
-REFRESH_RATE = 300000 -- 5 min
+REFRESH_RATE = 1000 -- 5 secs
 
 -- GPIO
 SDA_PIN = 3 -- GPIO_0
--- main.lua
+++ main.lua
@@ -1,6 +1,8 @@
 require('config')
 
 TOPIC = "/sensors/tsl2561/data"
+m = mqtt.Client(CLIENT_ID, 120, "", "")
+ip = wifi.sta.getip()
 
 function readLux()
     tsl2561 = require("tsl2561")
@@ -10,36 +12,42 @@ function readLux()
     rst["_0"] = ch0
     rst["_1"] = ch1    
     tsl2561 = nil
-    package.loaded["tsl2561"] = nil    
+    package.loaded["tsl2561"] = nil
     return rst
 end
-
--- Init client with keepalive timer 120sec
-m = mqtt.Client(CLIENT_ID, 120, "", "")
-
-ip = wifi.sta.getip()
 
 m:lwt("/offline", '{"message":"'..CLIENT_ID..'", "topic":"'..TOPIC..'", "ip":"'..ip..'"}', 0, 0)
 
 print("Connecting to MQTT: "..BROKER_IP..":"..BROKER_PORT.."...")
-m:connect(BROKER_IP, BROKER_PORT, 0, 1, function(conn)
-    print("Connected to MQTT: "..BROKER_IP..":"..BROKER_PORT.." as "..CLIENT_ID)
 
-    data = readLux()
-        LUX_1 = tonumber(data._1)
-        DATA = '{"mac":"'..wifi.sta.getmac()..'","ip":"'..ip..'",'
-        DATA = DATA..'"luminosity":"'..LUX_1..'","refresh":"'..REFRESH_RATE..'"}'
+m:connect(BROKER_IP, BROKER_PORT, 0, 1, function(conn)
+        print("Connected to MQTT: "..BROKER_IP..":"..BROKER_PORT.." as "..CLIENT_ID)
+
+        DATA = '{"mac":"'..wifi.sta.getmac()..'", "ip":"'..ip..'", "online":"true"}'
+
         m:publish(TOPIC, DATA, 0, 0, function(conn)
-            print(CLIENT_ID.." sending data: "..DATA.." to "..TOPIC)
-        end)
-        
-    tmr.alarm(1, REFRESH_RATE, 1, function()
+                print(CLIENT_ID.." sending online: "..DATA.." to "..TOPIC)
+            end)
+
         data = readLux()
-        LUX_1 = tonumber(data._1)
+        TMP_LUX_1 = tonumber(data._1)
         DATA = '{"mac":"'..wifi.sta.getmac()..'","ip":"'..ip..'",'
-        DATA = DATA..'"luminosity":"'..LUX_1..'","refresh":"'..REFRESH_RATE..'"}'        
+        DATA = DATA..'"luminosity":"'..TMP_LUX_1..'","refresh":"'..REFRESH_RATE..'"}'
+
         m:publish(TOPIC, DATA, 0, 0, function(conn)
-            print(CLIENT_ID.." sending data: "..DATA.." to "..TOPIC)
-        end)
+                print(CLIENT_ID.." sending data: "..DATA.." to "..TOPIC)
+            end)
+
+        tmr.alarm(1, REFRESH_RATE, 1, function()
+                data = readLux()
+                LUX_1 = tonumber(data._1)
+                if(TMP_LUX_1 ~= LUX_1) then
+                    TMP_LUX_1 = LUX_1
+                    DATA = '{"mac":"'..wifi.sta.getmac()..'","ip":"'..ip..'",'
+                    DATA = DATA..'"luminosity":"'..LUX_1..'","refresh":"'..REFRESH_RATE..'"}'   
+                    m:publish(TOPIC, DATA, 0, 0, function(conn)
+                            print(CLIENT_ID.." sending data: "..DATA.." to "..TOPIC)
+                        end)
+                end
+            end)
     end)
-end)
